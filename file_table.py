67d6baa57c14352e8e104749f29d57e1b340c5c5
Patrick White <whitep@vcu.edu>
2016-07-03 16:57:22 -0400

    adding some ipython stuff and a small file glob thing

--- file_table.py
+++ file_table.py
@@ -2,9 +2,15 @@ import os
 import pandas as pd
 
 root_path = '/srv'
+nfs_exports = ['movies', 'tv']
+names = []
 
-for root, dirs, files in os.walk(root_path):
-    for name in files:
-        print(os.path.join(root, name))
-    for name in dirs:
-        print(os.path.join(root, name))
+for export in nfs_exports:
+    for root, dirs, files in os.walk(root_path):
+        for name in files:
+            names.append(name)
+        #print(os.path.join(root, name))
+    #for name in dirs:
+        #print(os.path.join(root, name))
+
+print names
